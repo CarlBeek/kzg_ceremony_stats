65d24c8aa3a4588e647a984203bf615ba5e1d37e
Carl Beekhuizen <carl@ethereum.org>
2023-04-27 14:48:03 -0600

    Assume default ENS resolver to halve requests per address

--- process_ens.py
+++ process_ens.py
@@ -1,36 +1,44 @@
 import web3
+import requests
+import sys
 from tqdm import tqdm
 import pandas as pd
 
-def reverse_ens_lookup(w3: web3.Web3, address: str, block_number: int) -> str:
+DEFAULT_ENS_RESOLVER_ADDRESS = '0xA2C122BE93b0074270ebeE7f6b7292C7deB45047'
+
+
+def _get_resolver_contract(w3: web3.Web3, address: str) -> web3.contract.Contract:
+    # Public Resolver ABI (only including the 'name' function)
+    public_resolver_abi = [
+        {
+            "constant": True,
+            "inputs": [{"name": "node", "type": "bytes32"}],
+            "name": "name",
+            "outputs": [{"name": "", "type": "string"}],
+            "payable": False,
+            "stateMutability": "view",
+            "type": "function",
+        }
+    ]
+
+    # Create a contract instance for the ENS resolver
+    resolver_contract = w3.eth.contract(address=address, abi=public_resolver_abi)
+    return resolver_contract
+
+
+def reverse_ens_lookup(w3: web3.Web3, address: str, block_number: int, resolver_contract: web3.contract.Contract) -> str:
     ens = w3.ens
     reverse_name = f"{address[2:].lower()}.addr.reverse"
-    resolver_address = ens.resolver(reverse_name)
     ens_name = ''
-
-    if resolver_address:
-        # Public Resolver ABI (only including the 'name' function)
-        public_resolver_abi = [
-            {
-                "constant": True,
-                "inputs": [{"name": "node", "type": "bytes32"}],
-                "name": "name",
-                "outputs": [{"name": "", "type": "string"}],
-                "payable": False,
-                "stateMutability": "view",
-                "type": "function",
-            }
-        ]
-
-        # Create a contract instance for the ENS resolver
-        resolver_contract = w3.eth.contract(address=resolver_address.address, abi=public_resolver_abi)
-
-        # Call the 'name' function with the specified block number
-        ens_name_hash = ens.namehash(reverse_name)
-        try:
-            ens_name = resolver_contract.functions.name(ens_name_hash).call(block_identifier=block_number)
-        except web3.exceptions.BadFunctionCallOutput:  # Some ENS names are badly formatted and this causes the above function to crash
-            pass
+    # Call the 'name' function with the specified block number
+    ens_name_hash = ens.namehash(reverse_name)
+    try:
+        ens_name = resolver_contract.functions.name(ens_name_hash).call(block_identifier=block_number)
+    except web3.exceptions.BadFunctionCallOutput:  # Some ENS names are badly formatted and this causes the above function to crash
+        pass
+    except requests.exceptions.HTTPError as e:
+        print(f'RPC error: {e}')
+        sys.exit(1)
     return ens_name
 
 def update_missing_ens(w3: web3.Web3, participants_df: pd.DataFrame, save_path: str, save_int: int, block: int) -> pd.DataFrame:
@@ -40,9 +48,10 @@ def update_missing_ens(w3: web3.Web3, participants_df: pd.DataFrame, save_path: 
     missing_data_df = missing_data_df[missing_data_df['address'].notna()]
     num_missing = missing_data_df.shape[0]
     print(f'{num_missing} ENS names missing')
+    resolver_contract = _get_resolver_contract(w3, DEFAULT_ENS_RESOLVER_ADDRESS)
     for i, row in tqdm(missing_data_df.iterrows(), total=num_missing):
         address = row['address']
-        participants_df.loc[i, 'ens'] = reverse_ens_lookup(w3, address, block)
+        participants_df.loc[i, 'ens'] = reverse_ens_lookup(w3, address, block, resolver_contract)
         if i % save_int == 0:
             participants_df.to_pickle(save_path)
             participants_df.head()
